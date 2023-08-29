022c289a29e20018907524197daf33e95466d56f
Enzo Firmino Campanari <114703471+FIRMINOenzo@users.noreply.github.com>
2023-08-29 12:51:20 +0000

    feat: adding task entity

---
+++ todo/src/main/java/com/todolist/todo/entities/Task.java
@@ -0,0 +1,95 @@
+package com.todolist.todo.entities;
+
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="tasks")
+public class Task {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "tas_id")
+    private Long id;
+
+    @Column(name = "tas_title", columnDefinition = "TEXT")
+    private String title;
+
+    @Column(name = "tas_desc", columnDefinition = "TEXT")
+    private String description;
+
+    @Column(name = "tas_status")
+    private boolean status;
+
+    public Task() {
+    }
+
+    public Task(Long id, String title, String description, boolean status) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Task other = (Task) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    
+}
